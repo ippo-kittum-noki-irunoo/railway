e3dcb48fdd4352e80ff9efc690615cbe573d1e3c
CrazyDeveloperTG <119148554+CrazyDeveloperTG@users.noreply.github.com>
2023-01-22 22:15:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/Jiyad777/Railway-try.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
