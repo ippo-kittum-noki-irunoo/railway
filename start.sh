f533e943d7f7e3529e247536780f2178cc85d03d
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-02-14 21:29:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jiyad777/Railway-try.git /EvaMaria
+  git clone https://github.com/Jiyad777/Railway-try.git /Railway-try
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO"
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Railway-try
 fi
-cd /EvaMaria
+cd /Railway-try
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
